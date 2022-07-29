2ef780323c264da3193f8b67ad5ae5eee549e235
Peach97 <90569344+Peach97@users.noreply.github.com>
2022-07-29 04:24:19 -0400

    bottom drawer component and hamburger functionality

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,36 @@ import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 
 export const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#00000035",
+    },
+  },
   typography: {
     fontFamily: ["Montserrat", "sans-serif"].join(","),
   },
 });
 //Montserrat Font Family global
 
+theme.typography.h3 = {
+  fontSize: "3rem",
+  "@media (max-width:600px)": {
+    fontSize: "2rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "2rem",
+  },
+};
+theme.typography.h1 = {
+  fontSize: "6rem",
+  "@media (max-width:600px)": {
+    fontSize: "5rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "5rem",
+  },
+};
+//media queries for h1/h3
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
