0594c2b9a3157e7259aba61570cb650008df0746
Cam <90569344+Peach97@users.noreply.github.com>
2022-10-09 11:37:24 -0400

    Update TakeoverPreview.js

--- components/Three/TakeoverPods/TakeoverPreview.js
+++ components/Three/TakeoverPods/TakeoverPreview.js
@@ -4,7 +4,7 @@ import { useGLTF } from "@react-three/drei";
 export function Model(props, ref) {
   const group = useRef();
   const { nodes, materials } = useGLTF(
-    "models/takeoverpreview.glb"
+    "/models/TakeoverPreview.glb"
   );
   return (
     <group ref={group} {...props} dispose={null}>
@@ -30,5 +30,5 @@ export function Model(props, ref) {
   );
 }
 
-useGLTF.preload("models/takeoverpreview.glb");
+useGLTF.preload("/models/TakeoverPreview.glb");
 export default forwardRef(Model);
