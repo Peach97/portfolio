f4d912bd42397568ef47659022a4bf7c9987c6ab
Cam <90569344+Peach97@users.noreply.github.com>
2022-10-13 00:37:53 -0400

    Update Floor.js

--- components/Experience/World/Floor.js
+++ components/Experience/World/Floor.js
@@ -26,9 +26,9 @@ export default class Floor {
   setCircles() {
     const geometry = new THREE.CircleGeometry(500, 3200);
     const material = new THREE.MeshStandardMaterial({ color: "#D2CBBD" });
-    const material2 = new THREE.MeshStandardMaterial({ color: "#151413" });
+    const material2 = new THREE.MeshStandardMaterial({ color: "#080808" });
     const material3 = new THREE.MeshStandardMaterial({ color: "#2A2A29" });
-    const material4 = new THREE.MeshStandardMaterial({ color: "#080808" });
+    const material4 = new THREE.MeshStandardMaterial({ color: "#151413" });
     //Circle geometry color
 
     this.circleFirst = new THREE.Mesh(geometry, material);
